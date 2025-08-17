6cf727c85c0b2f0ca9c4a58a0c75888554029822
IJustReallyLikeSpecksOfDust <max.falconerware@education.nsw.gov.au>
2025-08-17 21:47:33 +1000

    That was a long coding session.
    Spent a loooong time doing the following:
    Gathering all of the non-CPI data into consdata.csv
    Making basically everything for main.py
    Making a few things for data_module.py.
    And basically everything else that doesn't require pandas and mpl, so that I can have complete focus on those in the last week.

--- data_module.py
+++ data_module.py
@@ -1,3 +1,81 @@
-#it's like main.py but even more bleak.
+#here's the imports
 import pandas as pd
 import matplotlib
+import random
+
+#okay first time to define some variables
+thedata = pd.read_csv('data/consdata.csv') #this should be reading the data but i dont know if it is. i'm not getting anything 
+passwordentered = False
+correctpass = 'cheeseburgertempo'
+
+#print the full dataset
+def fulldataset(): 
+    print(thedata)
+    #I really have no idea if this is the output I need to give but it took me an embarassing amount of time to figure out that you needed to do it as a print, so I'm going to say this is fine.
+
+#visualisation options. gonna take some time
+def visualisations():
+    print('Function not finished. Sorry.')
+
+#finds and filters data
+def datafindfilter():
+    print('Function not finished. Sorry.')    
+
+#a simple password protection function. usage of global allows it to work between functions if i code this in a good way (with a loop)
+def passwordprotection():
+    global passwordentered
+    global correctpass
+    if passwordentered == False:
+        guess = input('Input the password. ')
+        if guess == correctpass:
+            passwordentered = True
+        else:
+            print('(Not actually) Sorry to tell you this, but that password is wrong.')
+
+#change values. this seems hard to code
+def makemods():
+    passwordprotection()
+    if passwordentered == True:
+        print('Function not finished. Sorry.')
+
+#save the things changed by makemods. may just be integrated into makemods.
+def updatefile():
+    passwordprotection()
+    if passwordentered == True:
+        print('Function not finished. Sorry.')
+
+#this'll either just be a pointless for-fun function or something that works between program uses, depending on if i can make the password write to another document.
+def changepass():
+    passwordprotection()
+    if passwordentered == True:
+        print('Function not finished. Sorry.')
+
+#this exists because I wanted to refresh myself on python coding
+def ropasc():
+    userchoice = input('Please type Rock, Paper, or Scissors; ').lower()
+    interfacechoice = str(random.randint(1,3))
+    if userchoice == 'rock' and interfacechoice == '1':
+        print('We both chose Rock, as such it is a tie. Not much of a landslide...')
+    elif userchoice == 'rock' and interfacechoice == '2':
+        print('Haha, I won! You chose Rock and I chose Paper. Yay, for once I get some excitement!')
+    elif userchoice == 'rock' and interfacechoice == '3':
+        print('Well... You chose Rock and I chose Scissors. Am I not a program? Why can I even lose, this is dumb.')
+    elif userchoice == 'paper' and interfacechoice == '1':
+        print("Well... You chose Paper and I chose Rock. That's kinda rude y'know?")
+    elif userchoice == 'paper' and interfacechoice == '2':
+        print('We both chose Paper, as such it is a tie. This is starting to remind me of an office...')
+    elif userchoice == 'paper' and interfacechoice == '3':
+        print("Haha, I won! You chose Paper and I chose Scissors! This is the most fun I've ever had.")
+    elif userchoice == 'scissors' and interfacechoice == '1':
+        print("Haha, I won! You chose Scissors and I chose Rock! Get... Rocked? Whatever, I won, ha-ha!")
+    elif userchoice == 'scissors' and interfacechoice == '2':
+        print("Well... You chose Scissors and I chose Paper. T'ch, you just got lucky.")
+    elif userchoice == 'scissors' and interfacechoice == '3':
+        print("We both chose Scissors, as such it is a tie. Maybe we should cut the tie.")
+    elif userchoice == 'gun':
+        print("What are you, 6? There's no 'Gun' choice in Rock Paper Scissors.")
+    else:
+        print("Are you stupid? That's not a valid choice, learn to spell! Wait, sorry that was rude, you probably just made a typo...")
+
+
+#the other options will be local to main.py
--- main.py
+++ main.py
@@ -1,4 +1,59 @@
-#im sure there'll be stuff here eventually
-
+#hey look there's stuff here
+#setup imports and variables and stuff
 import pandas as pd
 import matplotlib
+import random
+import time
+from data_module import (
+    fulldataset,
+    visualisations,
+    datafindfilter,
+    ropasc,
+    makemods,
+    updatefile,
+    changepass
+)
+TheSnoreVariable = True
+
+
+print("Hi, I'm the U.I. for the Videogame Console Dataset.")
+
+#long bit of print statements for information's sake. Might find a to toggle.
+def UI():
+    global TheSnoreVariable
+    print('These are the functions you can access through me:')
+    print('1. View Full Dataset')
+    print('2. Visualise Data')
+    print('3. Find Data via Search or Filter')
+    print('4. Modify existing Data(Req. Password)')
+    print('5. Save Modifications (Req. Password)')
+    print('6. Change Password (Req. Password)')
+    print("7. View my Creator's Conclusions")
+    print('8. Play Rock Paper Scissors')
+    print('9. Exit Program')
+    what = input('What would you like to do? Input 1-9: ').strip() #found this cool thing to reduce typos
+    if what == '1':
+        fulldataset()
+    elif what == '2':
+        visualisations()
+    elif what == '3':
+        datafindfilter()
+    elif what == '4':
+        makemods()
+    elif what == '5':
+        updatefile()
+    elif what == '6':
+        changepass()
+    elif what == '7':
+        print('A wordy explanation of my person opinion after having done all this. Will be added... when I have done all this')
+    elif what == '8':
+        ropasc()
+    elif what == '9':
+        print('Bye-Bye!')
+        TheSnoreVariable = False
+    else:
+        print("Apparently that's not a valid input... You should try again, you probably typed something wrong. Either that or there's a bug.")
+
+while TheSnoreVariable == True:
+    UI()
+    time.sleep(1)
